747df9d74c32f3521e6a71ea399eb3b2b8d89db5
Andy <andy@siteworx.com>
2011-01-12 21:02:02 -0500

    hey neat, a working webpage. <3 node

--- server.js
+++ server.js
@@ -1,21 +1,37 @@
 var http = require('http'),
 	sys = require('sys'),
 	redis = require('redis'); 
+	log = require('./lib/logger'); 
 	client = redis.createClient().on('error', function(err) {
-		console.log('poop went kablamo: '+err);
+		log.error('poop went kablamo: '+err);
 	});
 
 client.get('/', function(err, res) {
-	console.log(err, res);
+	if(res === null) {
+		client.set('/', {title:'hello'}, function() {
+			console.log('Welcome to Ray-Frame. Your home page has been automatically added to the database.');
+		});
+	} else if(err) {
+		log.error('Aw snap, everything is terrible: '+err);
+	} else {
+		runServer();
+	}
 });
 
-http.createServer(function (req, res) {
-	var path = req.url.split('/');
+function runServer() {
+	http.createServer(function (req, res) {
+		var path = req.url.split('/');
 
-	client.set('test', 'hello', redis.print);
+		client.get(req.url, function(err, content) {
+			if(content) {
+				res.writeHead(200, {'Content-Type': 'text/plain'});
+				res.end('Hello World\n');
+			} else {
+				res.writeHead(404, {'Content-Type': 'text/plain'});
+				res.end('UR KRAP IS MISSN');
+			}
+		});
+	}).listen(8080, "127.0.0.1");
 
-	res.writeHead(200, {'Content-Type': 'text/plain'});
-	res.end('Hello World\n');
-}).listen(8080, "127.0.0.1");
-
-console.log('Server running!');
+	console.log('Server running!');
+};
