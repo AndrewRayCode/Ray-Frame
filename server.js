1bd568ebce5a0eb197eeff010ce88bcbd226911c
Andy <andy@siteworx.com>
2011-01-12 20:52:13 -0500

    woah redis works on my windows machine

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
-var http = require('http');
+var http = require('http'),
+	sys = require('sys'),
+	redis = require('redis'); 
+	client = redis.createClient().on('error', function(err) {
+		console.log('poop went kablamo: '+err);
+	});
+
 http.createServer(function (req, res) {
+	var path = req.url.split('/');
+
+	client.set('test', 'hello', redis.print);
+
 	res.writeHead(200, {'Content-Type': 'text/plain'});
 	res.end('Hello World\n');
 }).listen(8080, "127.0.0.1");
