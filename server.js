54291617d494774dfa68dd1f0979839c2f21b972
Andy <andy@siteworx.com>
2011-01-12 21:29:50 -0500

    saving and loading file from redis based on request. VERY COOLIO

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 var http = require('http'),
 	sys = require('sys'),
 	redis = require('redis'); 
-	log = require('./lib/logger'); 
+	log = require('./lib/logger'),
+	fs = require('fs'),
 	client = redis.createClient().on('error', function(err) {
 		log.error('poop went kablamo: '+err);
 	});
 
-client.get('/', function(err, res) {
+log.log_level = 'info';
+client.hgetall('/', function(err, res) {
 	if(res === null) {
-		client.set('/', {title:'hello'}, function() {
+		client.HMSET('/', {template:'index.html', title:'hello'}, function() {
 			console.log('Welcome to Ray-Frame. Your home page has been automatically added to the database.');
+			runServer();
 		});
 	} else if(err) {
 		log.error('Aw snap, everything is terrible: '+err);
@@ -20,18 +23,25 @@ client.get('/', function(err, res) {
 
 function runServer() {
 	http.createServer(function (req, res) {
-		var path = req.url.split('/');
-
-		client.get(req.url, function(err, content) {
+		//var path = req.url.split('/');
+		client.hgetall(req.url, function(err, content) {
 			if(content) {
 				res.writeHead(200, {'Content-Type': 'text/plain'});
-				res.end('Hello World\n');
+				res.end(parseTemplate(content.template));
+			} else if (err) {
+				log.error('uh oh: '+err);
+				res.writeHead(500, {'Content-Type': 'text/plain'});
+				res.end('Internal server errrrrrror');
 			} else {
 				res.writeHead(404, {'Content-Type': 'text/plain'});
 				res.end('UR KRAP IS MISSN');
 			}
 		});
 	}).listen(8080, "127.0.0.1");
-
 	console.log('Server running!');
 };
+
+function parseTemplate(file) {
+	var f = fs.readFileSync('templates/'+file);
+	return f;
+}
