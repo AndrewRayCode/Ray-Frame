6e14afee62c57e7f54fccf54c0d8120746d98ccc
Andy <andy@siteworx.com>
2011-01-12 22:59:06 -0500

    now loads template files with data, and I am getting more and more annoyed at how you cannot put an exclamation mark in commit messages

--- server.js
+++ server.js
@@ -5,12 +5,13 @@ var http = require('http'),
 	fs = require('fs'),
 	client = redis.createClient().on('error', function(err) {
 		log.error('poop went kablamo: '+err);
-	});
+	}),
+	isAdmin = 1;
 
 log.log_level = 'info';
 client.hgetall('/', function(err, res) {
-	if(res === null) {
-		client.HMSET('/', {template:'index.html', title:'hello'}, function() {
+	if(res !== null) {
+		client.HMSET('/', {template:'index.html', title:'hello', welcome_msg:'Welcome to this website!'}, function() {
 			console.log('Welcome to Ray-Frame. Your home page has been automatically added to the database.');
 			runServer();
 		});
@@ -26,14 +27,14 @@ function runServer() {
 		//var path = req.url.split('/');
 		client.hgetall(req.url, function(err, content) {
 			if(content) {
-				res.writeHead(200, {'Content-Type': 'text/plain'});
-				res.end(parseTemplate(content.template));
+				res.writeHead(200, {'Content-Type': 'text/html'});
+				res.end(serveTemplate(content));
 			} else if (err) {
 				log.error('uh oh: '+err);
-				res.writeHead(500, {'Content-Type': 'text/plain'});
+				res.writeHead(500, {'Content-Type': 'text/html'});
 				res.end('Internal server errrrrrror');
 			} else {
-				res.writeHead(404, {'Content-Type': 'text/plain'});
+				res.writeHead(404, {'Content-Type': 'text/html'});
 				res.end('UR KRAP IS MISSN');
 			}
 		});
@@ -41,7 +42,35 @@ function runServer() {
 	console.log('Server running!');
 };
 
-function parseTemplate(file) {
-	var f = fs.readFileSync('templates/'+file);
-	return f;
+function serveTemplate(obj) {
+	return parseTemplate(obj);
+	try {
+		var f = fs.readFileSync('compiled/'+obj.template);
+		return f;
+	} catch(e) {
+		return parseTemplate(obj);
+	}
+}
+
+var modelReplaces = /{{\S+?}}/g;
+
+function parseTemplate(obj) {
+	try {
+		var f = fs.readFileSync('templates/'+obj.template).toString();
+
+		var matches = f.match(modelReplaces), s = matches.length;
+		while(s--) {
+			f = f.replace(matches[s], getData(matches[s], obj));
+		}
+
+		fs.writeFileSync('compiled/'+obj.template, f);
+		return f;
+	} catch(e) {
+		log.error('Template not found for `'+obj+'`: '+e);
+	}
+}
+
+function getData(str, obj) {
+	var val = obj[str.substring(2, str.length-2)] || '';
+	return val;
 }
