b8c343fc64c2d47e24aa85dab29ef0a2e857dd86
Andy Ray <andy@siteworx.com>
2011-02-15 02:04:43 -0500

    working on test server utility method, nothing to see here yet, move along

--- core/test/utils.js
+++ core/test/utils.js
@@ -1,12 +1,51 @@
 var log = require('../lib/logger'),
+    http = require('http'),
     server = require('../../server');
 
-exports.requestURL = function(thing, url) {
+exports.requestURL = function(assert, thing, hitMe, cb) {
+    var rayframe;
     function run() {
+        var client = http.createClient(thing.server_port, 'localhost');
 
+        var request = client.request(hitMe.method || 'GET', hitMe.url).on('response', function(response) {
+            var chunks = [], length = 0;
+
+            request.connection.on('error', function(err) {
+                assert.done();
+            });
+            response.on('data', function(chunk) {
+                chunks.push(chunk);
+                length += chunk.length;
+            });
+            function done(){
+                request.connection.end();
+
+                var buff = new Buffer(length),
+                    offset = 0;
+                for(c in chunks){
+                    chunks[c].copy(buff, offset, 0);
+                    offset += chunks[c].length;
+                }
+
+                cb(rayframe, buff);
+            }
+            response.on('end', done);
+            response.connection.on('close', done);
+        });
+        request.connection.on('error', function(err) {
+            log.error('Error on request to server: ',err);
+            assert.done();
+        });
+        request.end(req.body);
     }
+
     // This is a config object
     if(!thing.createServer) {
-        server.createServer(thing, run);
+        thing.db_name = thing.db_name || 'rayframe-test'; 
+        rayframe = server.createServer(thing, run);
+    // We are reusing a server instance
+    } else {
+        rayframe = thing;
+        run();
     }
 };
