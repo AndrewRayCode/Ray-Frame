024d4ed28e9594c822bf4930480842e134d599a9
Andy Ray <andy@siteworx.com>
2011-02-17 01:55:48 -0500

    need to write test for adding item on list sub page adds it to the wrong list

--- core/test/suites/server_tests.js
+++ core/test/suites/server_tests.js
@@ -30,5 +30,26 @@ module.exports = testutils.testCase({
                 assert.done();
             });
         });
-    }
+    }/*,
+    // todo: chain addlistitem > get it > savelistitem > load new page > addlistitem > savelistitem > make sure added to homepage list, not new page list
+    'test add item to list on sub page': function(assert) {
+        var self = this;
+        assert.expect(3);
+        testutils.requestURL(self, assert, {}, {
+            method:'POST',
+            url:'/access/update',
+            body: {current_id:'root', current_url_id:'~', field:'global:message', value:'I am a new value'}
+        }, function(server, response) {
+            // Test that the save method worked
+            var j = JSON.parse(response);
+            assert.equals(j.status, 'success');
+            assert.equals(j.new_value, 'I am a new value');
+
+            // Test that if we hit the page again we get the new value
+            testutils.requestURL(self, assert, server, {url:'/'}, function(server, response) {
+                assert.ok(response.indexOf('I am a new value') > -1, 'New value was not found on page');
+                assert.done();
+            });
+        });
+    }*/
 });
